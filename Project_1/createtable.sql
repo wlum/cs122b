df785213c4f58f25b51dd4b2297522d604a287ec
Colin Son <ccson@uci.edu>
2016-03-30 18:14:31 -0700

    Added more to createtable.sql

--- Project_1/createtable.sql
+++ Project_1/createtable.sql
@@ -8,4 +8,20 @@ title VARCHAR(100) NOT NULL,
 year INT NOT NULL, 
 director VARCHAR(100) NOT NULL, 
 PRIMARY KEY (id)
-)
+);
+
+CREATE TABLE stars(
+id INT NOT NULL, 
+first_name VARCHAR(50) NOT NULL, 
+last_name VARCHAR(50) NOT NULL, 
+dob DATE, 
+photo_url VARCHAR(200), 
+PRIMARY KEY(id)
+); 
+
+CREATE TABLE stars_in_movies(
+star_id INT, 
+movie_id INT, 
+FOREIGN KEY (star_id) REFERENCES stars(id), 
+FOREIGN KEY (movie_id) REFERENCES movies(id)
+);
